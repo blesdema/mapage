4f4503998ef1f467f5b64d4b920a71ca6243aa96
blesdema <78177756+blesdema@users.noreply.github.com>
2021-07-09 11:03:11 -0400

    index.php et batch start_server
    Juste cliquer sur start_server
    et aller sur un navigateur à http://localhost:8082

---
+++ start_server.bat
@@ -0,0 +1,11 @@
+@echo off
+color F5
+
+
+REM call ./config/prepare.bat
+
+set php_path=C:\Users\%USERNAME%\progs\php7.3.11\php.exe
+
+echo "phppath est :"
+echo "Lancement du serveur de developpement :  %php_path%"
+%php_path% -S localhost:8082
